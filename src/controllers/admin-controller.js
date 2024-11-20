ea234cc531cc805b28fa9c0a04f802dda1c60185
Your Name <you@example.com>
2024-11-20 23:00:37 +0500

    init

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -4,6 +4,46 @@ const bcrypt = require("bcrypt");
 const { createToken } = require("../helpers/jwt");
 const { prisma } = require("../helpers/db");
 
+const create = async (req, res, next) => {
+  try {
+    const { fullname, phone, password } = req.body;
+
+    const schema = Joi.object({
+        fullname: Joi.string().min(5).required(),
+        phone: Joi.string().min(5).required(),
+        password: Joi.string().min(5).required(),
+    });
+
+    const { error } = schema.validate({ fullname, phone, password });
+    if (error) {
+      return res.status(400).json({ status:400, message: error.details[0].message });
+    }
+
+    const existingUser = await prisma.users.findUnique({ where: { phone } });
+
+    if (existingUser) {
+      return res.status(401).json({ status: 401, message: "This Phone is already taken!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await prisma.users.create({
+      data: {
+        fullname,
+        phone,
+        password: hashedPassword,
+        isAdmin: true,
+      },
+    });
+
+    const token = createToken({ id: newUser.id, isAdmin: newUser.isAdmin });
+
+    res.json({ status: 201, message: "Registration successful!", data: token });
+  } catch (error) {
+    next(error);
+  }
+}
+
 const login = async (req, res, next) => {
     try {
       const { phone, password } = req.body;
@@ -84,5 +124,6 @@ const changeUser = async (req, res, next) => {
 module.exports = {
     login,
     changeUser,
-    getUsers
+    getUsers,
+    create
 };
